5618fca01d0e65c12b78f481addceb1fc96338de
Martin Giger <martin@humanoids.be>
2020-03-15 15:11:17 +0100

    fix: public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/logo-builder/dist/',
     filename: 'build.js'
   },
   module: {
